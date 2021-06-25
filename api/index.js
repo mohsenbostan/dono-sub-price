55a5869c298185a5e33c35510de2d4a5b693f2ee
Mohsen Bostan <mohsenbostandev@gmail.com>
2021-06-26 01:51:21 +0430

    init

---
+++ api/index.js
@@ -0,0 +1,29 @@
+const app = require("express")();
+const axios = require("axios");
+
+app.get("/", async (req, res) => {
+  try {
+    const donoResponse = await axios.get(
+      "https://sub-api.dono.gg/v2/subscriptions/active"
+    );
+    const responseObject = {
+      status: "success",
+      tiers: [
+        donoResponse.data.data[0].price,
+        donoResponse.data.data[1].price,
+        donoResponse.data.data[2].price,
+      ],
+      message: `Tier 1: ${donoResponse.data.data[0].price} ✔ Tier 2: ${donoResponse.data.data[1].price} ✔ Tier 3: ${donoResponse.data.data[2].price} ✔`,
+    };
+
+    return res.json(responseObject);
+  } catch (err) {
+    return res.json({
+      status: "failed",
+      tiers: null,
+      message: null,
+    });
+  }
+});
+
+module.exports = app;
